17ae9a37a7f2c8de01f1fb68f6a5133420e979d4
rakuraku.musen <musen.rakuraku@gmail.com>
2018-08-22 07:02:27 +0000

    add column

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -60,7 +60,9 @@ class MessagesController < ApplicationController
   end
   
   def message_params
-    params.require(:message).permit(:content)
+    params.require(:message).permit(:content, :title)
   end
+  
+  
 
 end
